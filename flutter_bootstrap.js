c57814efc58ca4fe49ab502b9b6ffaaf7072e439
Aritra Choudhury <ac8@rgbsi.co.in>
2025-02-05 15:50:53 +0530

    Initial commit

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3499763624"
+    serviceWorkerVersion: "1711453855"
   }
 });
